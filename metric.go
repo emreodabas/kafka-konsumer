3bdd1010b18fb7738405ba898aaf0d1472d2a2b9
berru.karakas <berru.karakas@trendyol.com>
2024-12-30 13:01:22 +0300

    implement atomic increment for prometheus metrics

--- metric.go
+++ metric.go
@@ -1,7 +1,21 @@
 package kafka
 
+import "sync/atomic"
+
 type ConsumerMetric struct {
 	TotalUnprocessedMessagesCounter      int64
 	TotalProcessedMessagesCounter        int64
 	TotalErrorCountDuringFetchingMessage int64
+}
+
+func (cm *ConsumerMetric) IncrementTotalUnprocessedMessagesCounter() {
+	atomic.AddInt64(&cm.TotalUnprocessedMessagesCounter, 1)
+}
+
+func (cm *ConsumerMetric) IncrementTotalProcessedMessagesCounter() {
+	atomic.AddInt64(&cm.TotalProcessedMessagesCounter, 1)
+}
+
+func (cm *ConsumerMetric) IncrementTotalErrorCountDuringFetchingMessage() {
+	atomic.AddInt64(&cm.TotalErrorCountDuringFetchingMessage, 1)
 }
