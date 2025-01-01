4c476eb0210a8698c9bd878ac868d81c29568ee8
A.Samet İleri <sametileri07@gmail.com>
2025-01-01 20:48:43 +0300

    Revert "refactor: change prometheus primary metrics as atomic ones (#159)"
    This reverts commit 50f4fcc5d83f716b25a4291af1c800a3e0b8d3c9.

--- collector.go
+++ collector.go
@@ -55,21 +55,21 @@ func (s *MetricCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(
 		s.totalProcessedMessagesCounter,
 		prometheus.CounterValue,
-		float64(s.consumerMetric.totalProcessedMessagesCounter),
+		float64(s.consumerMetric.TotalProcessedMessagesCounter),
 		emptyStringList...,
 	)
 
 	ch <- prometheus.MustNewConstMetric(
 		s.totalUnprocessedMessagesCounter,
 		prometheus.CounterValue,
-		float64(s.consumerMetric.totalUnprocessedMessagesCounter),
+		float64(s.consumerMetric.TotalUnprocessedMessagesCounter),
 		emptyStringList...,
 	)
 
 	ch <- prometheus.MustNewConstMetric(
 		s.totalErrorCountDuringFetchingMessage,
 		prometheus.CounterValue,
-		float64(s.consumerMetric.totalErrorCountDuringFetchingMessage),
+		float64(s.consumerMetric.TotalErrorCountDuringFetchingMessage),
 		emptyStringList...,
 	)
 }
--- metric.go
+++ metric.go
@@ -1,19 +1,7 @@
 package kafka
 
 type ConsumerMetric struct {
-	totalUnprocessedMessagesCounter      int64
-	totalProcessedMessagesCounter        int64
-	totalErrorCountDuringFetchingMessage int64
-}
-
-func (cm *ConsumerMetric) IncrementTotalUnprocessedMessagesCounter() {
-	cm.totalUnprocessedMessagesCounter++
-}
-
-func (cm *ConsumerMetric) IncrementTotalProcessedMessagesCounter() {
-	cm.totalProcessedMessagesCounter++
-}
-
-func (cm *ConsumerMetric) IncrementTotalErrorCountDuringFetchingMessage() {
-	cm.totalErrorCountDuringFetchingMessage++
+	TotalUnprocessedMessagesCounter      int64
+	TotalProcessedMessagesCounter        int64
+	TotalErrorCountDuringFetchingMessage int64
 }
